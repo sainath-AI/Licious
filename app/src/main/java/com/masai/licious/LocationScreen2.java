fd6ea106eb7a1d72636d7c07eb48566640490cdb
Saveen <saveen047@gmail.com>
2021-07-21 20:37:21 +0530

    updated splashScreen

--- app/src/main/java/com/masai/licious/LocationScreen2.java
+++ app/src/main/java/com/masai/licious/LocationScreen2.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LocationScreen2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_location_screen2);
     }
 }
