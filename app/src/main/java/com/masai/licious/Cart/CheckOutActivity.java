f3e093dfcb613e148c705c556eda0596eaed8d60
Sainath Reddy <dukedustine123@gmail.com>
2021-07-24 11:20:19 +0530

    cart

---
+++ app/src/main/java/com/masai/licious/Cart/CheckOutActivity.java
@@ -0,0 +1,16 @@
+package com.masai.licious.Cart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.masai.licious.R;
+
+
+public class CheckOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_out);
+    }
+}
