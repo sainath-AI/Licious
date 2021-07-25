8b0366494f217be577b72bbaee8dd6718ef86697
Leena Sunkara <leenasunkara444@gmail.com>
2021-07-25 20:21:06 +0530

    login page layout update

--- app/src/main/java/com/masai/licious/activities/LoginScreenOne.java
+++ app/src/main/java/com/masai/licious/activities/LoginScreenOne.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
